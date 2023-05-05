0e754454f958b68f9be6a230eed0c2ae78b67a76
Artsiom Karakin <karakin2000@gmail.com>
2023-05-05 23:42:27 +0300

    Adderd TODO's and updated copyrights.

--- hosts2zone.c
+++ hosts2zone.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Artsiom <aning@protonmail.ch>
+ * Copyright (c) 2021, 2023 Artsiom Karakin <karakin2000@gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
